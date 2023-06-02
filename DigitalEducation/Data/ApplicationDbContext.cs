9eb8fb4e88299a8b4ec9adcd5eb442183b86f45c
confect1on <confection12543@gmail.com>
2023-06-02 21:19:36 +0700

    Initial commit

---
+++ DigitalEducation/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DigitalEducation.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalEducation.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public DbSet<Test> Tests { get; set; }
+    
+    public DbSet<Problem> Problems { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
