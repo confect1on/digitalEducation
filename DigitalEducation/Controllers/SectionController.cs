914fac9ca3e74cedb5c3bc179635fdb76b19632c
confect1on <confection12543@gmail.com>
2023-06-06 22:05:16 +0700

    implement section entity, partially implementing CRUD for section entity

---
+++ DigitalEducation/Controllers/SectionController.cs
@@ -0,0 +1,70 @@
+﻿using DigitalEducation.Data;
+using DigitalEducation.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalEducation.Controllers;
+
+public class SectionController : Controller
+{
+    private readonly ApplicationDbContext dbContext;
+
+    public SectionController(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await dbContext.Sections.ToListAsync());
+    }
+    
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(Section section)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
+        await dbContext.Sections.AddAsync(section);
+        await dbContext.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        var section = await GetSectionById(id);
+        if (section is null)
+        {
+            return NotFound();
+        }
+        return View(section);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteAsync(Section section)
+    {
+        dbContext.Sections.Remove(section);
+        await dbContext.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    private async Task<Section?> GetSectionById(int? id)
+    {
+        if (id is null)
+        {
+            return null;
+        }
+
+        return await dbContext.Sections.FirstOrDefaultAsync(s => s.Id == id);
+    }
+}
---
+++ DigitalEducation/Entities/Section.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace DigitalEducation.Entities;
+
+public class Section
+{
+    public int Id { get; set; }
+    
+    [DisplayName("Название")]
+    required public string Name { get; set; }
+
+    public ICollection<Subsection> Subsections { get; set; } = new List<Subsection>();
+}
