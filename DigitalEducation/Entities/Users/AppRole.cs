5207039153b9f6f1d6ccdece107d039a1a7334a5
confect1on <confection12543@gmail.com>
2023-06-24 00:21:12 +0700

    Add classes for auth

---
+++ DigitalEducation/Entities/Users/AppRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DigitalEducation.Entities.Users;
+
+public class AppRole : IdentityRole<Guid>
+{
+    
+}
---
+++ DigitalEducation/Entities/Users/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DigitalEducation.Entities.Users;
+
+public class AppUser : IdentityUser<Guid>
+{
+    
+}
