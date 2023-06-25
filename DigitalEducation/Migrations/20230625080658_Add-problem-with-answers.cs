49ab5ef186de7165cd4eaeb733b3066b782c29a0
confect1on <confection12543@gmail.com>
2023-06-26 00:07:25 +0700

    Add auth and migrations

---
+++ DigitalEducation/Migrations/20230625080658_Add-problem-with-answers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DigitalEducation.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addproblemwithanswers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProblemsWithAnswer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ProblemId = table.Column<int>(type: "int", nullable: false),
+                    UserAnswer = table.Column<string>(type: "longtext", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProblemsWithAnswer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProblemsWithAnswer_Problems_ProblemId",
+                        column: x => x.ProblemId,
+                        principalTable: "Problems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProblemsWithAnswer_ProblemId",
+                table: "ProblemsWithAnswer",
+                column: "ProblemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProblemsWithAnswer");
+        }
+    }
+}
---
+++ DigitalEducation/Migrations/20230625114014_Add-trainers-table.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DigitalEducation.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addtrainerstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TrainerId",
+                table: "Theory",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Trainers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ProblemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trainers_Problems_ProblemId",
+                        column: x => x.ProblemId,
+                        principalTable: "Problems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TrainerTheories",
+                columns: table => new
+                {
+                    TrainerId = table.Column<int>(type: "int", nullable: false),
+                    TheoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainerTheories", x => new { x.TheoryId, x.TrainerId });
+                    table.ForeignKey(
+                        name: "FK_TrainerTheories_Theory_TheoryId",
+                        column: x => x.TheoryId,
+                        principalTable: "Theory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TrainerTheories_Trainers_TrainerId",
+                        column: x => x.TrainerId,
+                        principalTable: "Trainers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Theory_TrainerId",
+                table: "Theory",
+                column: "TrainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trainers_ProblemId",
+                table: "Trainers",
+                column: "ProblemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainerTheories_TrainerId",
+                table: "TrainerTheories",
+                column: "TrainerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Theory_Trainers_TrainerId",
+                table: "Theory",
+                column: "TrainerId",
+                principalTable: "Trainers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Theory_Trainers_TrainerId",
+                table: "Theory");
+
+            migrationBuilder.DropTable(
+                name: "TrainerTheories");
+
+            migrationBuilder.DropTable(
+                name: "Trainers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Theory_TrainerId",
+                table: "Theory");
+
+            migrationBuilder.DropColumn(
+                name: "TrainerId",
+                table: "Theory");
+        }
+    }
+}
